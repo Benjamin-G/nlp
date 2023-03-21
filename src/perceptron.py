6e454033ecbdabd4ff10fef699e44dc340b21138
benjamin-g <brgeyer49@gmail.com>
2023-03-21 11:37:45 -0700

    feat: 1.1

---
+++ src/perceptron.py
@@ -0,0 +1,31 @@
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.linear_model import Perceptron
+
+if __name__ == "__main__":
+    categories = ["alt.atheism", "sci.med"]
+
+    train = fetch_20newsgroups(subset="train", categories=categories, shuffle=True)
+
+    perceptron = Perceptron(max_iter=100)
+
+    cv = CountVectorizer()
+    X_train_counts = cv.fit_transform(train.data)
+
+    tfidf_tf = TfidfTransformer()
+    X_train_tfidf = tfidf_tf.fit_transform(X_train_counts)
+
+    perceptron.fit(X_train_tfidf, train.target)
+
+    test_docs = ["Religion is widespread, even in modern times",
+                 "His kidney failed", "The pope is a controversial leader",
+                 "White blood cells fight off infections",
+                 "The reverend had a heart attack in church"]
+
+    X_test_counts = cv.transform(test_docs)
+    X_test_tfidf = tfidf_tf.transform(X_test_counts)
+
+    pred = perceptron.predict(X_test_tfidf)
+
+    for doc, category in zip(test_docs, pred):
+        print(f"{doc!r} => {train.target_names[category]}")
