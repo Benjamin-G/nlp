c3ba6aa2eee978dca772880bb969add278cebd32
benjamin-g <brgeyer49@gmail.com>
2023-03-22 18:16:40 -0700

    feat: ReLU example

--- src/mlp_example_movie.py
+++ src/mlp_example_movie.py
@@ -10,16 +10,32 @@ if __name__ == "__main__":
     # Load the data and map the tags to 1 and 0
     fields = ["text", "tag"]
     data = pd.read_csv(get_data_path("movie_review.csv"), usecols=fields)
-    data["label"] = data["tag"].map({"pos": 1, "neg": 0})
-    data = data.sample(n=1000)
-    docs = data["text"]
+    data["label"] = data["tag"].map({"pos": 1, "neg": 0}).astype("string")
+
+    dtypes = {
+        "text": "string",
+        "tag": "string",
+        "label": "string",
+    }
+
+    data = data.astype(dtypes)
+    print("Loaded data:")
+    print(data.info())
+
+    # Split the data into training and test sets
+    # my laptop does not have much memory
+    training_data = data.sample(frac=0.25)
+    rest_part_25 = data.drop(training_data.index)
+
+    # Preparing the training data
+    docs = training_data["text"]
 
     tokenizer = Tokenizer()
     tokenizer.fit_on_texts(docs)
 
     # Convert the text to a matrix of token counts
     X_train = tokenizer.texts_to_matrix(docs, mode="binary")
-    y_train = np_utils.to_categorical(data["label"])
+    y_train = np_utils.to_categorical(training_data["label"])
 
     # Build the model
     input_dim = X_train.shape[1]
@@ -28,10 +44,22 @@ if __name__ == "__main__":
     model = Sequential()
     model.add(Dense(128, input_dim=input_dim))
 
-    # add 10 layers
+    # add 10 layers sigmoid 
+    # We observe that the network doesn`t seem to learn at all: its validation accuracy (the 
+    # accuracy it attains on a held-out test portion of its training data during training) does not increase.
+    # for _ in range(10):
+    #     model.add(Activation("sigmoid"))
+    #     model.add(Dense(128))
+    # Epoch 10/10
+    # 29/29 - 0s - loss: 0.6973 - accuracy: 0.4933 - val_loss: 0.6969 - val_accuracy: 0.4600 - 
+    # 345ms/epoch - 12ms/step
+
+    # add 10 layers ReLU
     for _ in range(10):
-        model.add(Activation("sigmoid"))
+        model.add(Activation("relu"))
         model.add(Dense(128))
+    # Epoch 10/10 29/29 - 0s - loss: 4.2112e-05 - accuracy: 1.0000 - val_loss: 5.0735 - val_accuracy: 0.6000 - 
+    # 332ms/epoch - 11ms/step
 
     model.add(Dense(nb_classes))
     model.add(Activation("softmax"))
@@ -40,5 +68,5 @@ if __name__ == "__main__":
                   metrics=["accuracy"])
 
     print("Training...")
-    model.fit(X_train, y_train, epochs=10, batch_size=32, validation_split=0.1,
+    model.fit(X_train, y_train, epochs=10, batch_size=256, validation_split=0.1,
               shuffle=False, verbose=2)
