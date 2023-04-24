4d7026ec49c99062bd63c456219f6ef924563ed9
benjamin-g <brgeyer49@gmail.com>
2023-04-24 13:50:31 -0700

    feat: lstm_character_prediction.py working

--- src/lstm_character_prediction.py
+++ src/lstm_character_prediction.py
@@ -1,16 +1,23 @@
 import numpy as np
-from keras.layers import Dense, SimpleRNN, TimeDistributed
+from keras.layers import LSTM, Dense, TimeDistributed
 from keras.models import Sequential
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 
 def run():
-    data = ["character"]
+    np.random.seed(1234)
+
+    data = ["xyzaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaxyz",
+            "pqraaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaapqr"]
+
+    test_data = ["xyzaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaxyz",
+                 "pqraaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaapqr"]
+
     enc = LabelEncoder()
-    alphabet = np.array(list({c for w in data for c in w}))  #
+    alphabet = np.array(list({c for w in data for c in w}))
     enc.fit(alphabet)
     int_enc = enc.fit_transform(alphabet)
-    onehot_encoder = OneHotEncoder(sparse_output=False)
+    onehot_encoder = OneHotEncoder(sparse=False)
     int_enc = int_enc.reshape(len(int_enc), 1)
     onehot_encoder.fit_transform(int_enc)
 
@@ -25,25 +32,27 @@ def run():
     X_test = []
     y_test = []
 
-    test_data = ["character"]
-
     for w in test_data:
         for i in range(len(w) - 1):
             X_test.extend(onehot_encoder.transform([enc.transform([w[i]])]))
+            print(i, w[i], onehot_encoder.transform([enc.transform([w[i]])]))
             y_test.extend(onehot_encoder.transform([enc.transform([w[i + 1]])]))
 
-    sample_size = 256
+    sample_size = 512
     sample_len = len(X_train)
 
-    X_train = np.array([X_train * sample_size]).reshape(sample_size, sample_len, len(alphabet))
-    y_train = np.array([y_train * sample_size]).reshape(sample_size, sample_len, len(alphabet))
+    X_train = np.array([X_train * sample_size]).reshape(
+        sample_size, sample_len, len(alphabet))
+    y_train = np.array([y_train * sample_size]).reshape(
+        sample_size, sample_len, len(alphabet))
+
     test_len = len(X_test)
     X_test = np.array([X_test]).reshape(1, test_len, len(alphabet))
     y_test = np.array([y_test]).reshape(1, test_len, len(alphabet))
 
     model = Sequential()
     model.add(
-        SimpleRNN(
+        LSTM(
             units=100,
             input_dim=len(alphabet),
             # output_dim=100,
@@ -54,30 +63,27 @@ def run():
         TimeDistributed(
             Dense(
                 len(alphabet),
-                # output_dim=len(alphabet),
                 activation="sigmoid",
             ),
         ),
     )
-    model.compile(
-        loss="binary_crossentropy",
-        metrics=["accuracy"],
-        optimizer="adam",
-    )
-    model.fit(X_train, y_train, epochs=10, batch_size=32)
+    model.compile(loss="binary_crossentropy", metrics=["accuracy"], optimizer="adam")
 
-    # Encoded classes
-    print(list(enc.classes_))
+    n = 1
+    while True:
+        score = model.evaluate(X_test, y_test, batch_size=32)
+        print("[Iteration %d] score=%f" % (n, score[1]))
+        if score[1] == 1.0:
+            break
+        n += 1
+        model.fit(X_train, y_train, epochs=1, batch_size=32)
+
     preds = model.predict(X_test)[0]
     for p in preds:
         m = np.argmax(p)
-        # print(p)
-        # print(m)
-        print(enc.inverse_transform([m]))
+        print(enc.inverse_transform([m])[0])
 
     print(model.evaluate(X_test, y_test, batch_size=32))
-    # The two final numerical scores express the loss and accuracy of the classifier for the test data; the 1.0 
-    # indicates a 100% accuracy score.
 
 
 if __name__ == "__main__":
