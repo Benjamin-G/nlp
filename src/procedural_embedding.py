e5423884198523c6d2124b589100b27018801b9f
benjamin-g <brgeyer49@gmail.com>
2023-04-26 17:58:09 -0700

    refactor: move plot function

--- src/procedural_embedding.py
+++ src/procedural_embedding.py
@@ -1,49 +1,9 @@
-import matplotlib.pyplot as plt
-import numpy as np
 from keras import Sequential
 from keras.layers import Embedding
 from keras.utils import pad_sequences
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.manifold import TSNE
 
-
-def tsne_plot(model, max_words=100):
-    labels = []
-    tokens = []
-
-    n = 0
-    for word in model:
-        if n < max_words:
-            tokens.append(model[word])
-            labels.append(word)
-            n += 1
-
-    tsne_model = TSNE(
-        perplexity=40,
-        n_components=2,
-        init="pca",
-        n_iter=10000,
-        random_state=23,
-    )
-
-    new_values = tsne_model.fit_transform(np.array(tokens))
-
-    x = []
-    y = []
-    for value in new_values:
-        x.append(value[0])
-        y.append(value[1])
-
-    plt.figure(figsize=(8, 8))
-    for i in range(len(x)):
-        plt.scatter(x[i], y[i])
-        plt.annotate(labels[i],
-                     xy=(x[i], y[i]),
-                     xytext=(5, 2),
-                     textcoords="offset points",
-                     ha="right",
-                     va="bottom")
-    plt.show()
+from src.utils import tsne_plot
 
 
 def run():
