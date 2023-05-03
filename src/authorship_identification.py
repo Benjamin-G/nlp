1cb9644cf5ea50ffbbb71b8d149699164efcc894
benjamin-g <brgeyer49@gmail.com>
2023-05-03 16:40:20 -0700

    feat: run_authorship_verification

--- src/authorship_identification.py
+++ src/authorship_identification.py
@@ -3,12 +3,14 @@ from os import listdir
 from os.path import isfile, join
 
 import numpy as np
-from keras import Sequential
-from keras.layers import Convolution1D, Dense, Embedding, Flatten
+from keras import Input, Model, Sequential
+from keras import backend as K
+from keras.layers import LSTM, Convolution1D, Dense, Embedding, Flatten, Lambda
 from keras.utils import np_utils, pad_sequences
 from keras_preprocessing.text import hashing_trick
 from nltk import ngrams
 from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
 
 
 def create_label_dict_one_file(path):
@@ -418,5 +420,145 @@ def run_authorship_attribution():
     print("Accuracy: %f" % (accuracy * 100))
 
 
+def split_data(X, y, max_samples_per_author=10):
+    """
+    randomly samples pairs of documents from the output from vectorizeDocumentsBOW()—X (vectors) and y (labels
+    corresponding to authors)—and labels the pairs with a 1 if both members of the pair belong to the same author or
+    0 if not. :param X: :type X: :param y: :type y: :param max_samples_per_author: :type max_samples_per_author:
+    :return: :rtype:
+    """
+    X, y = shuffle(X, y, random_state=42)
+    authors_x = {}
+
+    for (x, y) in zip(X, y):
+        y = np.where(y == 1)[0][0]
+        if y in authors_x:
+            authors_x[y].append(x)
+        else:
+            authors_x[y] = [x]
+
+    X_left = []
+    X_right = []
+    y_lr = []
+
+    Done = {}
+    for author in authors_x:
+        nb_texts = len(authors_x[author])
+        nb_samples = min(nb_texts, max_samples_per_author)
+        left_docs = np.array(authors_x[author])
+        random_indexes = np.random.choice(left_docs.shape[0], nb_samples,
+                                          replace=False)
+        left_sample = np.array(authors_x[author])[random_indexes]
+        for other_author in authors_x:
+            if (other_author, author) in Done:
+                pass
+            Done[(author, other_author)] = 1
+
+            right_docs = np.array(authors_x[other_author])
+
+            nb_samples_other = min(len(authors_x[other_author]),
+                                   max_samples_per_author)
+            random_indexes_other = np.random.choice(right_docs.shape[0],
+                                                    nb_samples_other, replace=False)
+            right_sample = right_docs[random_indexes_other]
+
+            for (l, r) in zip(left_sample, right_sample):
+                X_left.append(l)
+                X_right.append(r)
+                if author == other_author:
+                    y_lr.append(1.0)
+                else:
+                    y_lr.append(0.0)
+
+    return np.array(X_left), np.array(X_right), np.array(y_lr)
+
+
+def exp_neg_manhattan_distance(x, y):
+    """
+    A distance function
+    exponentiated negative sum of differences is a good choice since it produces values in the range [0,1]
+    :param x: 
+    :type x: 
+    :param y: 
+    :type y: 
+    :return: 
+    :rtype: 
+    """
+    return K.exp(-K.sum(K.abs(x - y), axis=1, keepdims=True))
+
+
+def run_authorship_verification():
+    """
+     For instance, we could have two sister networks working on bag-of-words features, 
+     two on character n-grams, and so on, and combine all of them into one Siamese network.
+    :return: 
+    :rtype: 
+    """
+    train = "../data/pan12-training"
+    # test = "../data/pan12-testing"
+
+    labelDict = create_label_dict_one_file(train)
+
+    input_dim = 500  # 500-word blocks
+
+    X, y, vocab_len = vectorize_documents_BOW(train, labelDict, input_dim)
+    vocab_size = len(labelDict)
+
+    X_train, X_test, y_train, y_test = \
+        train_test_split(X, y, test_size=0.33, random_state=42)
+
+    X_train_left, X_train_right, y_train_lr = split_data(X_train, y_train, 20)
+    X_test_left, X_test_right, y_test_lr = split_data(X_test, y_test, 20)
+
+    """
+     We can use any architecture we like for the two sister networks, but in this case, 
+     we`ll opt for a simple long short-term memory (LSTM) architecture. 
+    """
+
+    left_input = Input(shape=(input_dim,), dtype="int32")
+    right_input = Input(shape=(input_dim,), dtype="int32")
+
+    embedding = Embedding(vocab_size, 300, input_length=input_dim)
+    encoded_left = embedding(left_input)
+    encoded_right = embedding(right_input)
+
+    nb_units = 10
+
+    lstm = LSTM(nb_units)
+    left_output = lstm(encoded_left)
+    right_output = lstm(encoded_right)
+
+    model_distance = Lambda(
+        function=lambda x:
+        exp_neg_manhattan_distance(x[0], x[1]), output_shape=lambda x: (x[0][0], 1),
+    )([left_output, right_output])
+
+    model = Model([left_input, right_input], [model_distance])
+
+    model.compile(
+        loss="mean_squared_error",
+        optimizer="adam",
+        metrics=["accuracy"],
+    )
+
+    print(model.summary())
+
+    nb_epochs = 10
+
+    model.fit(
+        [X_train_left, X_train_right],
+        y_train_lr,
+        batch_size=64,
+        epochs=nb_epochs,
+        validation_split=0.3,
+        verbose=2,
+    )
+    # model.evaluate([X_test_left, X_test_right], y_test_lr)
+
+    loss, accuracy = model.evaluate(
+        [X_test_left, X_test_right], y_test_lr, verbose=0)
+    print("Accuracy: %f" % (accuracy * 100))
+
+
 if __name__ == "__main__":
-    run_authorship_attribution()
+    run_authorship_verification()
