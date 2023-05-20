df65458abae08538172e0cdf21baa905ee2a1d3b
benjamin-g <brgeyer49@gmail.com>
2023-05-20 12:06:49 -0700

    feat: storing the new co-references knowledge graph

--- src/spacy_ner_schema.py
+++ src/spacy_ner_schema.py
@@ -1,3 +1,4 @@
+import neuralcoref
 import spacy
 
 from src.graphdb_base import GraphDBBase
@@ -8,8 +9,9 @@ class GraphBasedNLP(GraphDBBase):
 
     def __init__(self):
         super().__init__(database="spacyner")
-        spacy.prefer_gpu()
         self.nlp = spacy.load("en_core_web_sm")
+        coref = neuralcoref.NeuralCoref(self.nlp.vocab)
+        self.nlp.add_pipe(coref, name="neuralcoref")
         self.__text_processor = TextProcessor(self.nlp, self.get_session)
         self.create_constraints()
 
@@ -19,6 +21,7 @@ class GraphBasedNLP(GraphDBBase):
         self.execute_without_exception("CREATE CONSTRAINT ON (t:Sentence) ASSERT (t.id) IS NODE KEY")
         self.execute_without_exception("CREATE CONSTRAINT ON (l:AnnotatedText) ASSERT (l.id) IS NODE KEY")
         self.execute_without_exception("CREATE CONSTRAINT ON (l:NamedEntity) ASSERT (l.id) IS NODE KEY")
+        self.execute_without_exception("CREATE CONSTRAINT ON (l:Entity) ASSERT (l.type, l.id) IS NODE KEY")
 
     def tokenize_and_store(self, text, text_id, storeTag):
         docs = self.nlp.pipe([text])
@@ -26,11 +29,13 @@ class GraphBasedNLP(GraphDBBase):
             annotated_text = self.__text_processor.create_annotated_text(doc, text_id)
             spans = self.__text_processor.process_sentences(annotated_text, doc, storeTag, text_id)
             self.__text_processor.process_entities(spans, text_id)
+            self.__text_processor.process_coreference(doc, text_id)
+            self.__text_processor.build_entities_inferred_graph(text_id)
 
 
 if __name__ == "__main__":
     basic_nlp = GraphBasedNLP()
-    store_tag = True
+    store_tag = False
     sentences = [
         "Marie Curie received the Nobel Prize in Physics in 1903. " +
         "She became the first woman to win the prize and the first person—man or woman—to win the award twice.",
